5ba95a0496b80523c73a7bbaef978c52eaf2a746
Bill Caputo <bill@williamcaputo.com>
2016-04-25 18:15:51 -0500

    added screen back, cleaned out non-bootstrap cruft; added machine selector and load buttons

--- public/js/ssm.js
+++ public/js/ssm.js
@@ -14,9 +14,6 @@ function parseInstruction(code) {
   var matches = code.match(/^([0-9,A-F])([0-9,A-F])([0-9,A-F]{2})$/);
 }
 
-function genRow(id) {
-  return $('<div>').addClass('row').addClass('data-row').attr('id', id);
-}
 function genColumns(length, idPrefix, classes) {
   return _.map(_.range(length), function(columnNumber) {
     return $('<td>').addClass(classes).attr("id", idPrefix + sprintf('%X',columnNumber));
@@ -24,22 +21,19 @@ function genColumns(length, idPrefix, classes) {
 }
 
 function onReady() {
-  //var screenLabels = genRow("screen-H");
-  //screenLabels.append($("<div>").addClass("screen-label").text('*'));
-  //_.each(_.range(16), function(num) { 
-  //  screenLabels.append($("<div>").addClass("screen-label").text(sprintf("%X",num))); 
-  //});
+  var screenLabels = $('<tr>').append($('<th>'));
+  _.each(_.range(16), function(columnNumber) {
+    screenLabels.append($('<th>').addClass('lead').text(sprintf('%X',columnNumber)));
+  });
+  $('#screen-header').append(screenLabels);
 
-  //$('#screen').append(screenLabels);
-  //_.each(_.range(16), function(rowNumber) {
-  //  var rowId = rowNumber.toString(16).toLocaleUpperCase();
-  //  var row = genRow("V" + rowId);
-  //  row.append($("<div>").addClass("screen-label").text(sprintf("%X",rowNumber)));
-  //  _.each(genColumns(16,"V" + rowId, "pixel"), function(col) {
-  //    row.append(col);
-  //  });
-  //  $('#screen').append(row);
-  //});
+  _.each(_.range(8), function(rowNumber) {
+    var row = $('<tr>').append($('<th>').text(sprintf('%X',rowNumber)));
+    _.each(genColumns(16,"V" + sprintf('%X',rowNumber), "pixel"), function(col) {
+      row.append(col.text(" "));
+    });
+    $('#screen-body').append(row);
+  });
 
   var registerLabels = $('<tr>').append($('<th>').addClass('lead').text('PC'));
   var registerValues = $('<tr>').append($('<td>').text('00'));
@@ -47,28 +41,32 @@ function onReady() {
     registerLabels.append($('<th>').addClass('lead').text(sprintf('R%X',columnNumber)));
     registerValues.append($('<td>').text('0000'));
   });
-  $('#memheader').append(registerLabels).append(registerValues);
+  $('#mem-header').append(registerLabels).append(registerValues);
 
   _.each(_.range(16), function(rowNumber) {
     var rowId = sprintf('%02X',rowNumber * 16);
     var row = $('<tr>').append($('<th>').text(rowId));
-    _.each(genColumns(16,"M" + rowId, "memcell"), function(col) {
+    _.each(genColumns(16,"M" + rowId), function(col) {
       row.append(col.text("0000"));
     });
-    $('#membody').append(row);
+    $('#mem-body').append(row);
   });
 
-  setPixel("C4", packPixel(3,0,72));
-  setPixel("C5", packPixel(3,0,101));
-  setPixel("C6", packPixel(3,0,108));
-  setPixel("C7", packPixel(3,0,108));
-  setPixel("C8", packPixel(3,0,111));
+  setPixel("44", packPixel(3,0,72));
+  setPixel("45", packPixel(3,0,101));
+  setPixel("46", packPixel(3,0,108));
+  setPixel("47", packPixel(3,0,108));
+  setPixel("48", packPixel(3,0,111));
 
   var editor = CodeMirror.fromTextArea($('#text-editor')[0], {
     theme:'cobalt',
     lineNumbers: true,
     styleActiveLine: true,
     matchBrackets: true
+  });
+  $(".dropdown-menu li a").click(function(){
+    var selText = $(this).text();
+    $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
   });
 }
 
