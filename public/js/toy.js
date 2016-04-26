8da9469e4ab7de565de8ed1aa6c35c85b5a3c301
Bill Caputo <bill@williamcaputo.com>
2016-04-26 13:54:40 -0500

    added testing framework and moved resources locally; added favicon and attribution

--- public/js/toy.js
+++ public/js/toy.js
@@ -1,4 +1,12 @@
 function Toy() {
+//  this.compile(text) {
+//    var src = text.split("\n");
+//    if(src[0].substr(0,3) === "#pc")
+//    _.each(src, function(inst,idx) {
+//      $('#M' + (idx +10)).text(inst);
+//    });
+//
+//  }
   this.load = function(text) {
     _.each(text.split("\n"), function(inst,idx) {
       $('#M' + (idx +10)).text(inst);
